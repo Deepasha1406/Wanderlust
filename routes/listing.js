c417165c8f9f3b37f2a88ab82a0f9b3d9da30772
Deepasha1406 <deepasha1406@gmail.com>
2025-07-09 01:30:57 -0700

    modified listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -6,7 +6,7 @@ const {isLoggedIn,isOwner,validateListing} = require("../middleware.js");
 const { authorize } = require("passport");
 const listingController = require("../controllers/listings.js");
 const multer  = require('multer');
-const {storage} = require("../cloudconfig.js");
+const {storage} = require("../cloudConfig.js");
 const upload = multer({storage});
 
 //new route  => it should always be above /:id route
